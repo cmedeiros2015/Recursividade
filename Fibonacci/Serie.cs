40240feaa5c35023e159856298b92f634bacf98c
cmedeiros2015 <carloseduardo.demedeiros@gmail.com>
2015-06-14 00:14:33 -0300

    Primeiro Commit
    Código C# para sequência de Fibonacci

---
+++ Fibonacci/Serie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Serie
+    {
+        public int serie(int inicio, int limite) {
+            if (inicio < limite)
+            {
+                System.Console.WriteLine(inicio);
+                return serie(inicio + 1, limite);
+            }
+        }
+    }
+}
---
+++ Usa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonacci;
+
+namespace Usa
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int res,ini = 10,lim = 30,saida;
+            System.Console.WriteLine("Digite um valor: ");
+            int num = int.Parse(System.Console.ReadLine());
+            Fib fb = new Fib();
+            res = fb.cFib(num);
+            System.Console.WriteLine(res);
+            Serie s = new Serie();
+            saida = s.serie(ini, lim);
+            System.Console.WriteLine(saida);
+            System.Console.ReadKey();
+        }
+    }
+}
